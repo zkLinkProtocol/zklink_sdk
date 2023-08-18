d289ec7daae3346625fa249c5bcd46f4abc917dc
Fred <fred@zklink.org>
2023-08-18 16:52:48 +0800

    add zklink private key

--- common/src/crypto/mod.rs
+++ common/src/crypto/mod.rs
@@ -1,34 +1,27 @@
 //! Utils for signing zksync transactions.
 //! This crate is compiled into wasm to be used in `zksync.js`.
 
+pub mod signature;
 mod utils;
+pub mod zklink_private_key;
+
+use crate::crypto::utils::set_panic_hook;
+use crate::crypto::zklink_private_key::privkey_from_slice;
+pub use franklin_crypto::bellman::pairing::bn256::{Bn256 as Engine, Fr};
+use franklin_crypto::rescue::bn256::Bn256RescueParams;
+use franklin_crypto::{
+    alt_babyjubjub::{edwards, fs::FsRepr, AltJubjubBn256, FixedGenerators},
+    bellman::pairing::ff::{PrimeField, PrimeFieldRepr},
+    eddsa::{PublicKey, Seed, Signature as EddsaSignature},
+    jubjub::JubjubEngine,
+};
+use wasm_bindgen::prelude::*;
 
 const PACKED_POINT_SIZE: usize = 32;
 const PACKED_SIGNATURE_SIZE: usize = 64;
 
-pub use franklin_crypto::bellman::pairing::bn256::{Bn256 as Engine, Fr};
-use franklin_crypto::rescue::bn256::Bn256RescueParams;
-
 pub type Fs = <Engine as JubjubEngine>::Fs;
-
-thread_local! {
-    pub static JUBJUB_PARAMS: AltJubjubBn256 = AltJubjubBn256::new();
-    pub static RESCUE_PARAMS: Bn256RescueParams = Bn256RescueParams::new_checked_2_into_1();
-}
-
-use wasm_bindgen::prelude::*;
-
-use franklin_crypto::{
-    alt_babyjubjub::{edwards, fs::FsRepr, AltJubjubBn256, FixedGenerators},
-    bellman::pairing::ff::{PrimeField, PrimeFieldRepr},
-    eddsa::{PrivateKey, PublicKey, Seed, Signature as EddsaSignature},
-    jubjub::JubjubEngine,
-};
-
 pub type Signature = EddsaSignature<Engine>;
-
-use crate::crypto::utils::set_panic_hook;
-use sha2::{Digest, Sha256};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -36,87 +29,27 @@ use sha2::{Digest, Sha256};
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen(start)]
+thread_local! {
+    pub static JUBJUB_PARAMS: AltJubjubBn256 = AltJubjubBn256::new();
+    pub static RESCUE_PARAMS: Bn256RescueParams = Bn256RescueParams::new_checked_2_into_1();
+}
+
 /// This method initializes params for current thread, otherwise they will be initialized when signing
 /// first message.
+#[wasm_bindgen(start)]
 pub fn zksync_crypto_init() {
     JUBJUB_PARAMS.with(|_| {});
     RESCUE_PARAMS.with(|_| {});
     set_panic_hook();
 }
 
-#[wasm_bindgen(js_name = privateKeyFromSeed)]
-pub fn private_key_from_seed(seed: &[u8]) -> Result<Vec<u8>, JsValue> {
-    if seed.len() < 32 {
-        return Err(JsValue::from_str("Seed is too short"));
-    };
-
-    let sha256_bytes = |input: &[u8]| -> Vec<u8> {
-        let mut hasher = Sha256::new();
-        hasher.input(input);
-        hasher.result().to_vec()
-    };
-
-    let mut effective_seed = sha256_bytes(seed);
-
-    loop {
-        let raw_priv_key = sha256_bytes(&effective_seed);
-        let mut fs_repr = FsRepr::default();
-        fs_repr
-            .read_be(&raw_priv_key[..])
-            .expect("failed to read raw_priv_key");
-        if Fs::from_repr(fs_repr).is_ok() {
-            return Ok(raw_priv_key);
-        } else {
-            effective_seed = raw_priv_key;
-        }
-    }
-}
-
-fn read_signing_key(private_key: &[u8]) -> Result<PrivateKey<Engine>, JsValue> {
-    let mut fs_repr = FsRepr::default();
-    fs_repr
-        .read_be(private_key)
-        .map_err(|_| JsValue::from_str("couldn't read private key repr"))?;
-    Ok(PrivateKey::<Engine>(
-        Fs::from_repr(fs_repr).expect("couldn't read private key from repr"),
-    ))
-}
-
-fn privkey_to_pubkey_internal(private_key: &[u8]) -> Result<PublicKey<Engine>, JsValue> {
-    let p_g = FixedGenerators::SpendingKeyGenerator;
-
-    let sk = read_signing_key(private_key)?;
-
-    Ok(JUBJUB_PARAMS.with(|params| PublicKey::from_private(&sk, p_g, params)))
-}
-
+/// get the public key hash from public key
 #[wasm_bindgen(js_name = pubKeyHash)]
 pub fn pub_key_hash(pubkey: &[u8]) -> Result<Vec<u8>, JsValue> {
     let pubkey = JUBJUB_PARAMS
         .with(|params| PublicKey::read(pubkey, params))
         .map_err(|_| JsValue::from_str("couldn't read public key"))?;
     Ok(utils::pub_key_hash(&pubkey))
-}
-
-#[wasm_bindgen]
-pub fn private_key_to_pubkey_hash(private_key: &[u8]) -> Result<Vec<u8>, JsValue> {
-    Ok(utils::pub_key_hash(&privkey_to_pubkey_internal(
-        private_key,
-    )?))
-}
-
-#[wasm_bindgen]
-pub fn private_key_to_pubkey(private_key: &[u8]) -> Result<Vec<u8>, JsValue> {
-    let mut pubkey_buf = Vec::with_capacity(PACKED_POINT_SIZE);
-
-    let pubkey = privkey_to_pubkey_internal(private_key)?;
-
-    pubkey
-        .write(&mut pubkey_buf)
-        .expect("failed to write pubkey to buffer");
-
-    Ok(pubkey_buf)
 }
 
 #[wasm_bindgen(js_name = "rescueHash")]
@@ -131,17 +64,17 @@ pub fn rescue_hash_orders(msg: &[u8]) -> Vec<u8> {
     utils::rescue_hash_orders(msg)
 }
 
-#[wasm_bindgen]
 /// We use musig Schnorr signature scheme.
 /// It is impossible to restore signer for signature, that is why we provide public key of the signer
 /// along with signature.
 /// [0..32] - packed public key of signer.
 /// [32..64] - packed r point of the signature.
 /// [64..96] - s poing of the signature.
+#[wasm_bindgen]
 pub fn sign_musig(private_key: &[u8], msg: &[u8]) -> Result<Vec<u8>, JsValue> {
     let mut packed_full_signature = Vec::with_capacity(PACKED_POINT_SIZE + PACKED_SIGNATURE_SIZE);
     let p_g = FixedGenerators::SpendingKeyGenerator;
-    let private_key = read_signing_key(private_key)?;
+    let private_key = privkey_from_slice(private_key)?;
 
     {
         let public_key =
@@ -223,45 +156,4 @@ fn deserialize_signature(bytes: &[u8]) -> Result<Signature, JsValue> {
         .map_err(|_| JsValue::from_str("Failed to parse signature"))?;
 
     Ok(Signature { r, s })
-}
-
-///
-/// func GenerateLayer2PrivateKeySeed(ethKey string) ([]byte, error) {
-/// 	msg := "Sign this message to create a key to interact with zkLink's layer2 services.\nNOTE: This application is powered by zkLink protocol.\n\nOnly sign this message for a trusted client!"
-///
-/// 	privateKey, _ := crypto.HexToECDSA(strings.ToLower(ethKey))
-/// 	hash := crypto.Keccak256([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(msg), msg)))
-/// 	signature, err := crypto.Sign(hash, privateKey)
-/// 	signature[64] += 27
-/// 	if err != nil {
-/// 		return nil, err
-/// 	}
-///
-/// 	return signature, nil
-/// }
-
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_sign() {
-        // let s = "be725250b123a39dab5b7579334d5888987c72a58f4508062545fe6e08ca94f4";
-        // let priv_key_raw = read_signing_key(&hex::decode(s).unwrap()).unwrap();
-        // let seed = hex::decode(s).unwrap();
-        let seed = hex::decode("4e676849675aa768792f3f0cb7a993f55caea561635ceea06f8e0fc70c62b8e57adffc47ebc1a2c2377fce88d76dc51d78f7749ac12d9f8fc7bfe0ce694175c21c").unwrap();
-        let priv_key_raw = private_key_from_seed(&seed).unwrap();
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        println!("{priv_key_raw:?}");
-        let pub_key = private_key_to_pubkey(&priv_key_raw).unwrap();
-        println!("pubkey calc  : {}", hex::encode(&pub_key));
-        println!("pubkey expect: 5d29e3296af85d962dbfae8f1fbdc295e5d57eb4fddb8186de7c06be8df768ac");
-        let pub_key_hash = private_key_to_pubkey_hash(&priv_key_raw).unwrap();
-        println!("new pubkey hash: {}", hex::encode(pub_key_hash));
-        let msg :[u8; 32] = [166, 250, 15, 177, 151, 97, 14, 156, 48, 11, 76, 17, 0, 13, 17, 66, 241, 162, 16, 186, 55, 222, 87, 213, 109, 241, 137, 184, 73, 251, 47, 208];
-        let sig = sign_musig( &priv_key_raw, &msg).unwrap();
-        let verify_result = verify_musig(&msg, &sig);
-        assert!(verify_result.is_ok());
-    }
 }
---
+++ common/src/crypto/zklink_private_key.rs
@@ -0,0 +1,151 @@
+use crate::crypto::{utils, Engine, Fs, JUBJUB_PARAMS, PACKED_POINT_SIZE};
+use ethers::core::k256::ecdsa::{Signature as EcdsaSignature, SigningKey};
+use ethers::prelude::k256::ecdsa::RecoveryId;
+use franklin_crypto::alt_babyjubjub::fs::FsRepr;
+use franklin_crypto::alt_babyjubjub::FixedGenerators;
+use franklin_crypto::bellman::pairing::ff::PrimeField;
+use franklin_crypto::bellman::PrimeFieldRepr;
+use franklin_crypto::eddsa::PrivateKey as FLPrivateKey;
+use franklin_crypto::eddsa::PublicKey as FlPublickKey;
+use sha2::{Digest, Sha256};
+use sha3::Keccak256;
+use wasm_bindgen::prelude::*;
+
+pub type PrivateKey = FLPrivateKey<Engine>;
+pub type PublicKey = FlPublickKey<Engine>;
+
+const SIGN_MESSAGE: &str =
+    "Sign this message to create a key to interact with zkLink's layer2 services.\nNOTE: This application is powered by zkLink protocol.\n\nOnly sign this message for a trusted client!";
+
+fn sign_hash(data: &str) -> Vec<u8> {
+    let hash = format!("\x19Ethereum Signed Message:\n{}{data}", data.len());
+    hash.as_bytes().to_vec()
+}
+
+fn person_sign(sign_key: &SigningKey, msg: &str) -> Result<(Vec<u8>, u8), JsValue> {
+    let hash = sign_hash(msg);
+    let hash_digest = Keccak256::new_with_prefix(hash);
+    let (signature, recover_id): (EcdsaSignature, RecoveryId) = sign_key
+        .sign_digest_recoverable(hash_digest)
+        .map_err(|_| JsValue::from_str("invalid secp256 private key"))?;
+    Ok((signature.to_vec(), recover_id.to_byte()))
+}
+
+/// https://stackoverflow.com/questions/69762108/implementing-ethereum-personal-sign-eip-191-from-go-ethereum-gives-different-s
+fn privkey_seed(eth_hex_private_key: &str) -> Result<Vec<u8>, JsValue> {
+    let raw_sign_key: [u8; 32] = hex::decode(eth_hex_private_key)
+        .map_err(|_| JsValue::from_str("invalid secp256 private key"))?
+        .try_into()
+        .map_err(|_| JsValue::from_str("invalid secp256 private key"))?;
+    let sign_key = SigningKey::from_slice(&raw_sign_key)
+        .map_err(|_| JsValue::from_str("invalid secp256 private key"))?;
+    let (mut signature, id) = person_sign(&sign_key, SIGN_MESSAGE)?;
+    signature.push(id + 27);
+    Ok(signature)
+}
+
+/// create the private key from seed
+#[wasm_bindgen(js_name = privateKeyFromSeed)]
+pub fn private_key_from_seed(seed: &[u8]) -> Result<Vec<u8>, JsValue> {
+    if seed.len() < 32 {
+        return Err(JsValue::from_str("Seed is too short"));
+    };
+
+    let sha256_bytes = |input: &[u8]| -> Vec<u8> {
+        let mut hasher = Sha256::new();
+        hasher.update(input);
+        hasher.finalize().to_vec()
+    };
+
+    let mut effective_seed = sha256_bytes(seed);
+
+    loop {
+        let raw_priv_key = sha256_bytes(&effective_seed);
+        let mut fs_repr = FsRepr::default();
+        fs_repr
+            .read_be(&raw_priv_key[..])
+            .expect("failed to read raw_priv_key");
+        if Fs::from_repr(fs_repr).is_ok() {
+            return Ok(raw_priv_key);
+        } else {
+            effective_seed = raw_priv_key;
+        }
+    }
+}
+
+/// Zklink private key from the hex formatted eth private key
+pub fn privkey_from_eth_privkey(eth_hex_private_key: &str) -> Result<Vec<u8>, JsValue> {
+    let seed = privkey_seed(eth_hex_private_key)?;
+    private_key_from_seed(&seed)
+}
+
+/// Zklink private key from a raw scalar serialized as a byte slice
+pub fn privkey_from_slice(private_key: &[u8]) -> Result<PrivateKey, JsValue> {
+    let mut fs_repr = FsRepr::default();
+    fs_repr
+        .read_be(private_key)
+        .map_err(|_| JsValue::from_str("couldn't read private key repr"))?;
+    let private_key = FLPrivateKey::<Engine>(
+        Fs::from_repr(fs_repr).expect("couldn't read private key from repr"),
+    );
+    Ok(private_key)
+}
+
+fn privkey_to_pubkey_internal(private_key: &[u8]) -> Result<PublicKey, JsValue> {
+    let p_g = FixedGenerators::SpendingKeyGenerator;
+    let sk = privkey_from_slice(private_key)?;
+    Ok(JUBJUB_PARAMS.with(|params| PublicKey::from_private(&sk, p_g, params)))
+}
+
+/// get the public key from private key
+#[wasm_bindgen(js_name = privateKeyToPubkey)]
+pub fn privkey_to_pubkey(private_key: &[u8]) -> Result<Vec<u8>, JsValue> {
+    let mut pubkey_buf = Vec::with_capacity(PACKED_POINT_SIZE);
+
+    let pubkey = privkey_to_pubkey_internal(private_key)?;
+
+    pubkey
+        .write(&mut pubkey_buf)
+        .expect("failed to write pubkey to buffer");
+
+    Ok(pubkey_buf)
+}
+
+/// get the public key hash from private key
+#[wasm_bindgen(js_name = privateKeyToPubkeyHash)]
+pub fn privkey_to_pubkey_hash(private_key: &[u8]) -> Result<Vec<u8>, JsValue> {
+    Ok(utils::pub_key_hash(&privkey_to_pubkey_internal(
+        private_key,
+    )?))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::crypto::{privkey_to_pubkey, privkey_to_pubkey_hash, sign_musig, verify_musig};
+
+    #[test]
+    fn test_sign() {
+        let eth_private_key = "be725250b123a39dab5b7579334d5888987c72a58f4508062545fe6e08ca94f4";
+        let seed = privkey_seed(eth_private_key).unwrap();
+        assert_eq!(hex::encode(&seed), "4e676849675aa768792f3f0cb7a993f55caea561635ceea06f8e0fc70c62b8e57adffc47ebc1a2c2377fce88d76dc51d78f7749ac12d9f8fc7bfe0ce694175c21c");
+        let private_key_raw = privkey_from_eth_privkey(eth_private_key).unwrap();
+        let pub_key = privkey_to_pubkey(&private_key_raw).unwrap();
+        assert_eq!(
+            hex::encode(&pub_key),
+            "7b173e25e484eed3461091430f81b2a5bd7ae792f69701dcb073cb903f812510"
+        );
+        let pub_key_hash = privkey_to_pubkey_hash(&private_key_raw).unwrap();
+        assert_eq!(
+            hex::encode(pub_key_hash),
+            "d8d5fb6a6caef06aa3dc2abdcdc240987e5330fe"
+        );
+        let msg: [u8; 32] = [
+            166, 250, 15, 177, 151, 97, 14, 156, 48, 11, 76, 17, 0, 13, 17, 66, 241, 162, 16, 186,
+            55, 222, 87, 213, 109, 241, 137, 184, 73, 251, 47, 208,
+        ];
+        let sig = sign_musig(&private_key_raw, &msg).unwrap();
+        let verify_result = verify_musig(&msg, &sig);
+        assert!(verify_result.is_ok());
+    }
+}
