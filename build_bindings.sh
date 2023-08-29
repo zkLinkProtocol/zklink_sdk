e48e7a6b7f910697e54abdcdcbf63243de95da61
Fred <fred@zklink.org>
2023-08-29 10:42:21 +0800

    fix golang test code

--- build_bindings.sh
+++ build_bindings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+#set -euxo pipefail
 
 SCRIPT_DIR="${SCRIPT_DIR:-$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )}"
 ROOT_DIR="$SCRIPT_DIR"
@@ -15,10 +15,10 @@ function bindings() {
     uniffi-bindgen-go $1 --out-dir "$BINDINGS_DIR"
 }
 
-bindings bindings/crypto/src/crypto.udl
+bindings crypto/src/crypto.udl
 
 #pushd $BINDINGS_DIR/..
 #export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$LIB_DIR"
-#	CGO_LDFLAGS="-lzklink_crypto_binding -L$LIB_DIR -lm -ldl" \
+#	CGO_LDFLAGS="-lzklink_sdk -L$LIB_DIR -lm -ldl" \
 #	CGO_ENABLED=1 \
 #	go test -v
--- crypto/build.rs
+++ crypto/build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    uniffi::generate_scaffolding("./src/crypto.udl").unwrap();
+    uniffi_build::generate_scaffolding("./src/crypto.udl").unwrap();
 }
