86e85da023f0312c62c110f9a372d8269a647240
nickwest <nickwest@zklink.org>
2024-01-01 22:35:14 -0800

    remove is_error in WsResponse and down the bigdecimal version

--- provider/src/web_socket/proto/response.rs
+++ provider/src/web_socket/proto/response.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use warp::ws::Message;
 
 #[derive(Deserialize, Serialize)]
 pub struct RegisterResponse {
@@ -17,19 +16,4 @@ pub struct WsResponse<T> {
     pub error_code: usize,
     pub error_msg: String,
     pub id: usize,
-}
-
-impl<T> WsResponse<T>
-where
-    T: Serialize,
-{
-    pub fn is_error(&self) -> bool {
-        self.error_code == 0
-    }
-
-    pub fn ws_message(&self) -> Result<Message, serde_json::Error> {
-        let s = serde_json::to_string(self)?;
-        let msg = Message::text(s);
-        Ok(msg)
-    }
 }
