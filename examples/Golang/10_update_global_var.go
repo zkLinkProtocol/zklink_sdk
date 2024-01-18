2fefae997c7d08f19038a7444248eb40e0ae33f9
nickwest <nickwest@zklink.org>
2024-01-17 23:48:10 -0800

    gofmt all go files

--- examples/Golang/10_update_global_var.go
+++ examples/Golang/10_update_global_var.go
@@ -1,76 +1,76 @@
 package main
 
 import (
-	"net/http"
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"bytes"
-	"io/ioutil"
 	sdk "github.com/zkLinkProtocol/zklink_sdk/go_example/generated/uniffi/zklink_sdk"
+	"io/ioutil"
+	"net/http"
 )
 
 type RPCTransaction struct {
-     Id      int64             `json:"id"`
-     JsonRpc string            `json:"jsonrpc"`
-     Method  string            `json:"method"`
-     Params  []json.RawMessage `json:"params"`
+	Id      int64             `json:"id"`
+	JsonRpc string            `json:"jsonrpc"`
+	Method  string            `json:"method"`
+	Params  []json.RawMessage `json:"params"`
 }
 
 type SubmiterSignature struct {
-    PubKey string `json:"pubKey"`
-    Signature string `json:"signature"`
+	PubKey    string `json:"pubKey"`
+	Signature string `json:"signature"`
 }
 
 func HighLevelUpdateGlobalVar() {
-    privateKey := "0xbe725250b123a39dab5b7579334d5888987c72a58f4508062545fe6e08ca94f4"
-    parameter := sdk.ParameterFeeAccount {
-        AccountId: sdk.AccountId(0),
-    }
-    builder := sdk.UpdateGlobalVarBuilder {
-        sdk.ChainId(1),
-        sdk.SubAccountId(1),
-        parameter,
-        101,
-    }
+	privateKey := "0xbe725250b123a39dab5b7579334d5888987c72a58f4508062545fe6e08ca94f4"
+	parameter := sdk.ParameterFeeAccount{
+		AccountId: sdk.AccountId(0),
+	}
+	builder := sdk.UpdateGlobalVarBuilder{
+		sdk.ChainId(1),
+		sdk.SubAccountId(1),
+		parameter,
+		101,
+	}
 
-    tx := sdk.NewUpdateGlobalVar(builder)
-    signer, err := sdk.NewSigner(privateKey, sdk.L1SignerTypeEth{})
-    if err != nil {
-        return
-    }
+	tx := sdk.NewUpdateGlobalVar(builder)
+	signer, err := sdk.NewSigner(privateKey, sdk.L1SignerTypeEth{})
+	if err != nil {
+		return
+	}
 
-    // get submitter signature
-    zklinkTx := tx.ToZklinkTx()
-    submitterSignature, err := signer.SubmitterSignature(zklinkTx)
-    submitterSignature2, err := json.Marshal(SubmiterSignature {
-        PubKey: submitterSignature.PubKey,
-        Signature: submitterSignature.Signature,
-    })
+	// get submitter signature
+	zklinkTx := tx.ToZklinkTx()
+	submitterSignature, err := signer.SubmitterSignature(zklinkTx)
+	submitterSignature2, err := json.Marshal(SubmiterSignature{
+		PubKey:    submitterSignature.PubKey,
+		Signature: submitterSignature.Signature,
+	})
 
-	rpc_req := RPCTransaction {
+	rpc_req := RPCTransaction{
 		Id:      1,
 		JsonRpc: "2.0",
 		Method:  "sendTransaction",
 		Params: []json.RawMessage{
-		    []byte(zklinkTx),
-		    nil,
-		    submitterSignature2,
+			[]byte(zklinkTx),
+			nil,
+			submitterSignature2,
 		},
-    }
+	}
 	JsonTx, err := json.Marshal(rpc_req)
-	fmt.Println("UpdateGlobalVar rpc request:",  string(JsonTx))
+	fmt.Println("UpdateGlobalVar rpc request:", string(JsonTx))
 	// get the testnet url or main net url
 	zklinkUrl := sdk.ZklinkTestNetUrl()
 	response, err := http.Post(zklinkUrl, "application/json", bytes.NewBuffer(JsonTx))
 	if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer response.Body.Close()
-    body, _ := ioutil.ReadAll(response.Body)
-    fmt.Println(string(body))
+		fmt.Println(err)
+		return
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
 }
 
 func main() {
-    HighLevelUpdateGlobalVar()
+	HighLevelUpdateGlobalVar()
 }
--- examples/Golang/2_withdraw.go
+++ examples/Golang/2_withdraw.go
@@ -1,110 +1,111 @@
 package main
 
 import (
-	"net/http"
-	"math/big"
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"time"
-	"bytes"
-	"io/ioutil"
 	sdk "github.com/zkLinkProtocol/zklink_sdk/go_example/generated/uniffi/zklink_sdk"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"time"
 )
 
 type RPCTransaction struct {
-     Id      int64             `json:"id"`
-     JsonRpc string            `json:"jsonrpc"`
-     Method  string            `json:"method"`
-     Params  []json.RawMessage `json:"params"`
+	Id      int64             `json:"id"`
+	JsonRpc string            `json:"jsonrpc"`
+	Method  string            `json:"method"`
+	Params  []json.RawMessage `json:"params"`
 }
 
 type SubmiterSignature struct {
-    PubKey string `json:"pubKey"`
-    Signature string `json:"signature"`
+	PubKey    string `json:"pubKey"`
+	Signature string `json:"signature"`
 }
+
 func HighLevelWithdraw() {
-    privateKey := "0xbe725250b123a39dab5b7579334d5888987c72a58f4508062545fe6e08ca94f4"
+	privateKey := "0xbe725250b123a39dab5b7579334d5888987c72a58f4508062545fe6e08ca94f4"
 	accountId := sdk.AccountId(8300)
 	subAccountId := sdk.SubAccountId(4)
 	toChainId := sdk.ChainId(5)
-    toAddress := sdk.ZkLinkAddress("0xAFAFf3aD1a0425D792432D9eCD1c3e26Ef2C42E9")
-    l2SourceToken := sdk.TokenId(17)
-    l1TargetToken := sdk.TokenId(17)
+	toAddress := sdk.ZkLinkAddress("0xAFAFf3aD1a0425D792432D9eCD1c3e26Ef2C42E9")
+	l2SourceToken := sdk.TokenId(17)
+	l1TargetToken := sdk.TokenId(17)
 	amount := *big.NewInt(1000000)
 	fee := *big.NewInt(1000)
 	nonce := sdk.Nonce(1)
 	withdrawFeeRatio := uint16(50)
-    // get current timestamp
-    now := time.Now()
-    timestamp := sdk.TimeStamp(now.Unix())
+	// get current timestamp
+	now := time.Now()
+	timestamp := sdk.TimeStamp(now.Unix())
 
-    builder := sdk.WithdrawBuilder{
-        AccountId: accountId,
-        ToChainId: toChainId,
-        SubAccountId: subAccountId,
-        ToAddress: toAddress,
-        L2SourceToken: l2SourceToken,
-        L1TargetToken: l1TargetToken,
-        Amount: amount,
-        nil,
-        Fee: fee,
-        Nonce: nonce,
-        WithdrawToL1: true,
-        WithdrawFeeRatio: withdrawFeeRatio,
-        Timestamp: timestamp,
-    }
-    tx := sdk.NewWithdraw(builder)
-    signer, err := sdk.NewSigner(privateKey, sdk.L1SignerTypeEth{})
-    if err != nil {
-        return
-    }
-    txSignature, err := signer.SignWithdraw(tx, "USDT",nil,nil)
-    fmt.Println("tx signature: %s", txSignature)
-    if err != nil {
-        return
-    }
-    // get the eth signature
-    var layer1Signature []byte = nil;
-    if txSignature.Layer1Signature != nil {
-        layer1Signature = []byte(*txSignature.Layer1Signature)
-    }
+	builder := sdk.WithdrawBuilder{
+		AccountId:     accountId,
+		ToChainId:     toChainId,
+		SubAccountId:  subAccountId,
+		ToAddress:     toAddress,
+		L2SourceToken: l2SourceToken,
+		L1TargetToken: l1TargetToken,
+		Amount:        amount,
+		nil,
+		Fee:              fee,
+		Nonce:            nonce,
+		WithdrawToL1:     true,
+		WithdrawFeeRatio: withdrawFeeRatio,
+		Timestamp:        timestamp,
+	}
+	tx := sdk.NewWithdraw(builder)
+	signer, err := sdk.NewSigner(privateKey, sdk.L1SignerTypeEth{})
+	if err != nil {
+		return
+	}
+	txSignature, err := signer.SignWithdraw(tx, "USDT", nil, nil)
+	fmt.Println("tx signature: %s", txSignature)
+	if err != nil {
+		return
+	}
+	// get the eth signature
+	var layer1Signature []byte = nil
+	if txSignature.Layer1Signature != nil {
+		layer1Signature = []byte(*txSignature.Layer1Signature)
+	}
 
-    // create the submitter signature
-    zklinkTx := tx.ToZklinkTx()
-    submitterSignature, err := signer.SubmitterSignature(zklinkTx)
-    submitterSignature2, err := json.Marshal(SubmiterSignature {
-        PubKey: submitterSignature.PubKey,
-        Signature: submitterSignature.Signature,
-    })
-	rpc_req := RPCTransaction {
+	// create the submitter signature
+	zklinkTx := tx.ToZklinkTx()
+	submitterSignature, err := signer.SubmitterSignature(zklinkTx)
+	submitterSignature2, err := json.Marshal(SubmiterSignature{
+		PubKey:    submitterSignature.PubKey,
+		Signature: submitterSignature.Signature,
+	})
+	rpc_req := RPCTransaction{
 		Id:      1,
 		JsonRpc: "2.0",
 		Method:  "sendTransaction",
 		Params: []json.RawMessage{
-		    []byte(txSignature.Tx),
-		    layer1Signature,
-            submitterSignature2,
+			[]byte(txSignature.Tx),
+			layer1Signature,
+			submitterSignature2,
 		},
-    }
+	}
 
 	JsonTx, err := json.Marshal(rpc_req)
 	if err != nil {
-	    fmt.Println(err)
-	    return
+		fmt.Println(err)
+		return
 	}
-	fmt.Println("ChangePubKey rpc request:",  string(JsonTx))
+	fmt.Println("ChangePubKey rpc request:", string(JsonTx))
 	// get the testnet url or main net url
 	zklinkUrl := sdk.ZklinkTestNetUrl()
 	response, err := http.Post(zklinkUrl, "application/json", bytes.NewBuffer(JsonTx))
 	if err != nil {
-        fmt.Println(err)
-        return
-    }
-    defer response.Body.Close()
-    body, _ := ioutil.ReadAll(response.Body)
-    fmt.Println(string(body))
+		fmt.Println(err)
+		return
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(body))
 }
 
 func main() {
-    HighLevelWithdraw()
+	HighLevelWithdraw()
 }
