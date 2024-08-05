cb64c8571fb07d1577e18a30585619dca957a13c
semi <semi@zklink.org>
2024-08-05 19:53:31 +0800

    bundle flutter_web binding

--- bindings/dart/rust/src/frb_generated.rs
+++ bindings/dart/rust/src/frb_generated.rs
@@ -4801,3 +4801,10 @@ impl SseEncode for i32 {
 mod io;
 #[cfg(not(target_family = "wasm"))]
 pub use io::*;
+
+/// cbindgen:ignore
+#[cfg(target_family = "wasm")]
+#[path = "frb_generated.web.rs"]
+mod web;
+#[cfg(target_family = "wasm")]
+pub use web::*;
